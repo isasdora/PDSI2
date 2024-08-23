98fe06a3fb52113fa653871b88fd5b693abdc5eb
isasdora <isadoram.martins@hotmail.com>
2024-08-23 16:17:16 -0300

    primeiro

--- database.py
+++ database.py
@@ -1,14 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
-load_dotenv()  # Loads environment variables from .env file
-
-# user = "postgres"
-# password = "isadora"
-# database = "pds"
-# host = "localhost"
+load_dotenv()
 
 user = os.getenv("USER") 
 password = os.getenv("PASSWORD")
